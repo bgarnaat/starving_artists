6205ffba13bb7c0bf742426859dfe228c3fa55b5
Zach Simonson <zsimonson26@gmail.com>
2016-01-25 14:25:58 -0800

    started htmlgenerator, linked JS to html pages

---
+++ js/html_generator.js
@@ -0,0 +1,5 @@
+function displayEventCard(scheduledEvent) {
+  var searchResultsContainer = document.getElementById('searchResultsContainer');
+  document.createElement('article')
+  searchResultsContainer.appendChild('eventCard')
+}
