2e95860366cfaff39179056818767bf518d7f41e
Jeff Eng <jeff@jeffeng.me>
2016-01-26 15:54:39 -0800

    called function and generates neighborhoods

---
+++ js/hood.js
@@ -0,0 +1,11 @@
+function fillHoodDown() {
+  var hoodDrop = document.getElementById('input_area');
+  for (var i = 0; i < allNeighborhoods.length; i++) {
+    var optEl = document.createElement('option');
+    optEl.value = allNeighborhoods[i];
+    optEl.textContent = allNeighborhoods[i];
+    hoodDrop.appendChild(optEl);
+  }
+}
+
+fillHoodDown();
