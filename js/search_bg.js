425ca88b143829310cc5b11e2490620c90b8e084
Ben Garnaat <garnaat.ben@gmail.com>
2016-01-26 09:13:10 -0800

    switchtastic

--- js/search_bg.js
+++ js/search_bg.js
@@ -17,13 +17,13 @@ function eventHandlerSubmit(event) {
 
 function checkEvents(input) {
   scheduled_events.forEach(function(item, index, array) {
-    check_name = input.in_show !== ' '? input.in_show === item.show.show_name : true;
-    check_date = input.in_date !== '' ? (input.in_date >= item.date_start || input.in_date <= item.date_end) : true;
-    check_venue = input.in_venue !== '' ? input.in_venue === item.venue.venue_name : true;
-    check_type = input.in_type !== '' ? input.in_type === item.show.show_type : true;
-    check_low = item.price < 20 ? input.low === true : true; // if price < 20 do something
-    check_med = item.price >= 20 && item.price < 40 ? input.med === true : true; // if price >= 20 && price < 40 do this
-    check_high = ; // if >= 40 do all the things
+    var check_name = input.in_show !== ''? input.in_show === item.show.show_name : true;
+    var check_date = input.in_date !== '' ? (input.in_date >= item.date_start || input.in_date <= item.date_end) : true;
+    var check_venue = input.in_venue !== '' ? input.in_venue === item.venue.venue_name : true;
+    var check_type = input.in_type !== '' ? input.in_type === item.show.show_type : true;
+    var check_low = item.price < 20 ? input.low === true : true; // if price < 20 do something
+    var check_med = item.price >= 20 && item.price < 40 ? input.med === true : true; // if price >= 20 && price < 40 do this
+    var check_high = item.price >=40 ? item.high === true : true; // if >= 40 do all the things
 
     if (check_name && check_date && check_venue && check_type && check_low && check_med && check_high) {
       displayEventCart(item);
