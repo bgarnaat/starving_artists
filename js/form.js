4d3cec19dd1e4ec87cfcb34377ebfb711242bf41
Jeff Eng <jeff@jeffeng.me>
2016-01-25 17:09:10 -0800

    event listeners and function placeholders

--- js/form.js
+++ js/form.js
@@ -15,14 +15,14 @@ function addVenue() {
 
 }
 
-//Get input 
+//Get input
 var searchButton = document.getElementById('search');
 var addShowButton = document.getElementById('add_show');
-var resetFormButton = document.getElementById('resetForm');
+var resetFormButton = document.getElementById('reset_form');
 var addVenueButton = document.getElementById('add_venue');
 
 //Event Listeners for 'Add Show', 'Add Venue', 'Search',  and 'Reset Form' buttons
 searchButton.addEventListener('submit', searchFunction goes here);
 addShowButton.addEventListener('submit', addShowFunction goes here);
-resetFormButton.addEventListener('submit', resetFormFunction goes here);
+resetFormButton.addEventListener('click', resetFormFunction goes here);
 addVenueButton.addEventListener('submit', addVenueFunction goes here);
