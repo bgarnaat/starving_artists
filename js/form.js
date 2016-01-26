5e80a029c44d1fc486c816f74fd1c4a6edd48ebb
Jeff Eng <jeff@jeffeng.me>
2016-01-25 17:00:24 -0800

    updated addshows.html with add show button and reset form button. Also created event handlers and placeholders for functions

---
+++ js/form.js
@@ -0,0 +1,28 @@
+//addShow Function
+function addShow() {
+
+}
+//resetForm Function
+function resetForm() {
+
+}
+//search Function
+function search() {
+
+}
+//addVenue Function
+function addVenue() {
+
+}
+
+//Get input 
+var searchButton = document.getElementById('search');
+var addShowButton = document.getElementById('add_show');
+var resetFormButton = document.getElementById('resetForm');
+var addVenueButton = document.getElementById('add_venue');
+
+//Event Listeners for 'Add Show', 'Add Venue', 'Search',  and 'Reset Form' buttons
+searchButton.addEventListener('submit', searchFunction goes here);
+addShowButton.addEventListener('submit', addShowFunction goes here);
+resetFormButton.addEventListener('submit', resetFormFunction goes here);
+addVenueButton.addEventListener('submit', addVenueFunction goes here);
