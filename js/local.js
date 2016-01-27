83476b59886df0eb09f50b905ea0ccb65b52ccdd
Ben Garnaat <garnaat.ben@gmail.com>
2016-01-27 11:21:47 -0800

    mucked up a bunch of files.

--- js/local.js
+++ js/local.js
@@ -1,24 +1,26 @@
-  
-  var ls_event_data = localStorage.getItem('ls_event_data');
-  if(ls_event_data) {
-  scheduled_events = JSON.parse(ls_event_data);
-  } else {
-  console.log('local storage empty! initializing:');
-  localStorage.setItem('ls_event_data', JSON.stringify(scheduled_events));
-  }
+'use strict'
 
-  var ls_show_data = localStorage.getItem('ls_show_data');
-  if(ls_show_data) {
-  shows = JSON.parse(ls_show_data);
-  } else {
-  console.log('local storage empty! initializing:');
-  localStorage.setItem('ls_show_data', JSON.stringify(shows));
-  }
+var ls_event_data = 'ls_event_data';
+var ls_show_data = 'ls_show_data';
+var ls_venue_data = 'ls_venue_data';
 
-  var ls_venue_data = localStorage.getItem('ls_venue_data');
-  if(ls_venue_data) {
-  venues = JSON.parse(ls_venue_data);
+ls_initialize(ls_show_data, shows);
+ls_initialize(ls_venue_data, venues);
+ls_initialize(ls_event_data, scheduled_events);
+
+function ls_initialize(ls_name, src_object) {
+  var ls_file = localStorage.getItem(ls_name);
+  if (ls_file) {
+    console.log('Loading local storage file: ' + ls_name);
+    ls_read(ls_file, src_object)
   } else {
-  console.log('local storage empty! initializing:');
-  localStorage.setItem('ls_venue_data', JSON.stringify(venues));
+    console.log('Local storage empty!  Initializing local storage.');
+    ls_write(ls_name, src_object);
   }
+}
+function ls_read(ls_file, src_object) {
+  src_object = JSON.parse(ls_file);
+}
+function ls_write(ls_name, src_object) {
+  localStorage.setItem(ls_name, JSON.stringify(src_object));
+}
