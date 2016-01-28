1bfe81af82c8a11e858e0535693b8b044b16e9f0
Ben Garnaat <garnaat.ben@gmail.com>
2016-01-28 14:55:53 -0800

    asdgjugsg

--- js/addShow.js
+++ js/addShow.js
@@ -23,8 +23,11 @@ function fillTypeDropdown() {
   }
 }
 function add_show(event) {
-  console.log(event);
   event.preventDefault();
+  if (!event.target.show_name.value || !event.target.start_date.value || !event.target.end_date.value || !event.target.price.value || !event.target.time.value || !event.target.in_description.value || !event.target.venue.value || !event.target.in_type.value) {
+    return alert('Fields cannot be empty');
+  }
+  console.log('true');
   var new_show = {
     in_name: event.target.show_name.value,
     start_date: event.target.start_date.value,
